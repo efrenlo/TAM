3b9f91fe1c7c125ae323f73891731298429481cd
efrenlo <eflopezvi@ittepic.edu.mx>
2019-05-08 18:49:16 -0600

    Eventos

---
+++ Unidad4/U4_AnimatoEveEfren/app/src/main/java/mx/edu/ittepic/u4_animatoeveefren/MainActivity.java
@@ -0,0 +1,63 @@
+package mx.edu.ittepic.u4_animatoeveefren;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private RelativeLayout canvas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.faceIcon);
+        canvas = (RelativeLayout) findViewById(R.id.animationCanvas);
+    }
+
+    public void onButtonClick(View v) {
+
+        int screenHeight = canvas.getHeight();
+        int targetY = screenHeight - imageView.getHeight();
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(
+                imageView, "x", (canvas.getHeight()-150), 1)
+                .setDuration(4000);
+
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.start();
+
+        //animator.setInterpolator(new BounceInterpolator());
+        //animator.setInterpolator(new CycleInterpolator(3));
+        //animator.start();
+
+        //animator.start();
+    }
+    public void onButton1Click(View v){
+        ObjectAnimator animator = ObjectAnimator.ofFloat(
+                imageView, "x", (canvas.getHeight()-150), 1)
+                .setDuration(5000);
+        animator.setInterpolator(new BounceInterpolator());
+        animator.start();
+    }
+    public void onButton2Click(View v){
+        ObjectAnimator animator = ObjectAnimator.ofFloat(
+                imageView, "y", (canvas.getHeight()-250), 1)
+                .setDuration(4000);
+        animator.setInterpolator(new AnticipateOvershootInterpolator(2));
+        animator.start();
+    }
+
+}
